c9f53db28167d3c2ec4162ba357d318c45d22650
grayzone <grayzone137@gmail.com>
2017-05-03 16:45:58 +0800

    find adjoin data.

--- util/data.go
+++ util/data.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	"math/rand"
 	"strconv"
 	"time"
@@ -42,7 +43,7 @@ func DropZeroData(s string) [10][10]int {
 	return result
 }
 
-func IsEmptyArray(a [10]int) bool {
+func isEmptyArray(a [10]int) bool {
 	sum := 0
 	for _, v := range a {
 		sum += v
@@ -53,13 +54,106 @@ func IsEmptyArray(a [10]int) bool {
 	return false
 }
 
+func foundNCrossData(x int, y int, input [10][10]int) [4]int {
+	var result [4]int
+	if x == 0 {
+		result[0] = -1
+	}
+	if x == 9 {
+		result[1] = -1
+	}
+	if y == 0 {
+		result[2] = -1
+	}
+	if y == 9 {
+		result[3] = -1
+	}
+	if result[0] != -1 {
+		result[0] = input[x-1][y]
+	}
+	if result[1] != -1 {
+		result[1] = input[x+1][y]
+	}
+	if result[2] != -1 {
+		result[2] = input[x][y-1]
+	}
+	if result[3] != -1 {
+		result[3] = input[x][y+1]
+	}
+	return result
+}
+
+func checkNCrossData(x int, y int, input [10][10]int) ([10][10]int, bool) {
+	var result [10][10]int
+	crossData := foundNCrossData(x, y, input)
+	point := input[x][y]
+
+	if (point != crossData[0]) && (point != crossData[1]) && (point != crossData[2]) && (point != crossData[3]) {
+		return result, false
+	}
+	result[x][y] = 1
+	if point == crossData[0] {
+		result[x-1][y] = 1
+	}
+	if point == crossData[1] {
+		result[x+1][y] = 1
+	}
+	if point == crossData[2] {
+		result[x][y-1] = 1
+	}
+	if point == crossData[3] {
+		result[x][y+1] = 1
+	}
+	return result, true
+}
+
+func AdjoinData(input [10][10]int) [10][10]int {
+	var result [10][10]int
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 10; j++ {
+			point := input[i][j]
+			var x1, x2, y1, y2 int
+
+			if i == 0 {
+				x1 = -1
+			}
+			if i == 9 {
+				x2 = -1
+			}
+			if j == 0 {
+				y1 = -1
+			}
+			if j == 9 {
+				y2 = -1
+			}
+			if x1 != -1 {
+				x1 = input[i-1][j]
+			}
+			if x2 != -1 {
+				x2 = input[i+1][j]
+			}
+			if y1 != -1 {
+				y1 = input[i][j-1]
+			}
+			if y2 != -1 {
+				y2 = input[i][j+1]
+			}
+
+			fmt.Printf("point:%d, x1:%d, x2:%d, y1:%d, y2:%d\n", point, x1, x2, y1, y2)
+
+		}
+	}
+
+	return result
+}
+
 func LeftData(input [10][10]int) [10][10]int {
 	var result [10][10]int
 	index := 0
 	for i := 0; i < 10; i++ {
 		a := input[i]
-		beego.Debug("a=", a)
-		if IsEmptyArray(a) {
+		fmt.Println("a=", a)
+		if isEmptyArray(a) {
 			continue
 		}
 		result[index] = a
